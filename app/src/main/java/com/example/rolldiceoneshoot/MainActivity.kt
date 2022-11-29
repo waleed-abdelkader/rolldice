b93e5cedc6f47cfc44a0dbe597de2ec6bb02078a
waleed <waleed.a.abdelkader@gmail.com>
2022-11-29 15:27:06 -0800

    full version

--- app/src/main/java/com/example/rolldiceoneshoot/MainActivity.kt
+++ app/src/main/java/com/example/rolldiceoneshoot/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rolldiceoneshoot
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,15 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val  rollButton: Button =findViewById(R.id.button)
         rollButton.setOnClickListener{
+           // make a toast
             val toast = Toast.makeText(this,"بختك يا ابوبخيت",Toast.LENGTH_SHORT)
             toast.show()
             // initiat auctall dice
             val randomNo = Dice().roll()
             val  resutTextView: TextView =findViewById(R.id.ResultView)
+            // initiat a pic and put it after click
             resutTextView.text = randomNo.toString()
+            val diceImage: ImageView = findViewById(R.id.resultpPic)
 
+            if (randomNo==1) {
+                diceImage.setImageResource(R.drawable.dice_1)
+            } else if (randomNo==2){
+                diceImage.setImageResource(R.drawable.dice_2)
+            }else if (randomNo==3){
+                diceImage.setImageResource(R.drawable.dice_3)
+            }else if (randomNo==4){
+                diceImage.setImageResource(R.drawable.dice_4)
+            }else if (randomNo==5){
+                diceImage.setImageResource(R.drawable.dice_5)
+            }else   {
+                diceImage.setImageResource(R.drawable.dice_6)
+            }
         }
-
     }
 
     class Dice(){
